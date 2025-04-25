cffb74a03b4a48e9c2a4632bd1ac352bfedaafb7
Hadi-Akram-Ramadhan <kk.hadi.akram@gmail.com>
2025-04-26 01:12:09 +0700

    add bootstrap

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '03352cd0870c7905a09e8daa6255b0490f9da3bd',
+        'reference' => 'a0a976a549ff12b070a1d2eadec3d5ffa040cd0d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '03352cd0870c7905a09e8daa6255b0490f9da3bd',
+            'reference' => 'a0a976a549ff12b070a1d2eadec3d5ffa040cd0d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -108,6 +108,21 @@
             'install_path' => __DIR__ . '/../symfony/polyfill-php80',
             'aliases' => array(),
             'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.3.5',
+            'version' => '5.3.5.0',
+            'reference' => '85f23534bd2de8041354b297516cf21959091b31',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.3.5',
+            ),
         ),
         'vlucas/phpdotenv' => array(
             'pretty_version' => 'v5.6.1',
